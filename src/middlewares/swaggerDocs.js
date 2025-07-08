2640782a69fb88c3c2eaccdfe11491fffce90a5c
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-07-08 11:56:59 +0200

    fix Swagger

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,17 +1,17 @@
-// src/middlewares/swaggerDocs.js
-
 import createHttpError from "http-errors";
 import swaggerUI from "swagger-ui-express";
 import fs from "node:fs";
-
 import { SWAGGER_PATH } from "../constants/index.js";
 
-export const swaggerDocs = () => {
-  try {
-    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
-  } catch (err) {
-    return (req, res, next) =>
-      next(createHttpError(500, "Can't load swagger docs"));
-  }
-};
+let swaggerMiddleware;
+
+try {
+  const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH, "utf-8"));
+  swaggerMiddleware = swaggerUI.setup(swaggerDoc);
+} catch (err) {
+  console.error("❌ Ошибка загрузки swagger.json:", err.message);
+  swaggerMiddleware = (req, res, next) =>
+    next(createHttpError(500, "Can't load swagger docs"));
+}
+
+export const swaggerDocs = [swaggerUI.serve, swaggerMiddleware];
