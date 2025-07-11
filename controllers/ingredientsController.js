29e8fefe82da02aed5b3ddba217733b132af531d
Dina Khomenko <dinamastercs@gmail.com>
2025-07-11 12:19:49 +0200

    Add default parination values vis pagination schema validation.

--- controllers/ingredientsController.js
+++ controllers/ingredientsController.js
@@ -1,10 +1,15 @@
 import * as ingredientsService from "../services/ingredientsService.js";
 import controllerWrapper from "../helpers/controllerWrapper.js";
+import { paginationSchema } from "../schemas/paginationSchema.js";
+import HttpError from "../helpers/HttpError.js";
 
 const getAllIngredients = async (req, res, next) => {
-  const { page, limit, name } = req.query;
+  const { error, value } = paginationSchema.validate(req.query);
+  if (error) throw HttpError(400, error.message);
+  const { page, limit } = value;
+  const { name } = req.query;
   const result = await ingredientsService.getAllIngredients(page, limit, name);
   res.json(result);
 };
 
-export const getAllIngredientsWrapper = controllerWrapper(getAllIngredients); 
+export const getAllIngredientsWrapper = controllerWrapper(getAllIngredients);
